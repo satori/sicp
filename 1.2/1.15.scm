852cb0ff5339cb5cd74ab29443fa639583706aa6
Maxim Bublis <b@codemonkey.ru>
2015-01-07 23:18:14 +0700

    Add exercise 1.15 solution

---
+++ 1.2/1.15.scm
@@ -0,0 +1,47 @@
+; Copyright (C) 2015 by Maxim Bublis <b@codemonkey.ru>
+;
+; Permission is hereby granted, free of charge, to any person obtaining
+; a copy of this software and associated documentation files (the
+; "Software"), to deal in the Software without restriction, including
+; without limitation the rights to use, copy, modify, merge, publish,
+; distribute, sublicense, and/or sell copies of the Software, and to
+; permit persons to whom the Software is furnished to do so, subject to
+; the following conditions:
+;
+; The above copyright notice and this permission notice shall be
+; included in all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+; NONINFRINGMENT. IN NOT EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+(define (cube x)
+  (* x x x))
+
+(define (p x)
+  (- (* 3 x)
+     (* 4 (cube x))))
+
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+    angle
+    (p (sine (/ angle 3.0)))))
+
+(sine 12.15)
+;Value: -.39980345741334
+
+(p (sine 4.05))
+
+(p (p (sine 1.35)))
+
+(p (p (p (sine 0.45))))
+
+(p (p (p (p (sine 0.15)))))
+
+(p (p (p (p (p (sine 0.05))))))
+
+(p (p (p (p (p 0.05)))))
